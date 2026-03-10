a18cbf48e1a3d260a638b7806c37f5286925024a
ashifislam08-hue <ashif.islam08@gmail.com>
2026-03-10 12:34:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const reveals=document.querySelectorAll('.reveal')
-
-window.addEventListener('scroll',()=>{
-
-for(let i=0;i<reveals.length;i++){
-
-let windowHeight=window.innerHeight
-let elementTop=reveals[i].getBoundingClientRect().top
-
-if(elementTop < windowHeight-100){
-reveals[i].classList.add('active')
-}
-
-}
-
-})
+console.log("Ashif Islam & Associates website loaded.");
